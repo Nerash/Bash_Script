16f4fc70009013fd8865efdac3752145a2773d95
Nerash <bjorn.holm.hansen@gmail.com>
2013-12-10 14:47:31 +0100

    /home/nerash/skola/bash/

--- lab2.sh
+++ lab2.sh
@@ -1,6 +1,7 @@
 # Laboration2 use of echo doing the hole compendium.
 ## Don't forget to coment syntaxes you arn't using. 
 
+#First test
 #!bin/bash
 #Username="cake?"
 #echo -n "what are you?"
@@ -9,8 +10,31 @@
 #exit 0
 
 ## Note to self use small letters when you use echo otherwise it won't work.
-Username="Cake"
-read -t 10 -p "What are you?" Cake
-Username=${Cake:="secret"}
-echo -e "\nMY $Username!"
-exit 0
+#================================
+## Second Test
+#Username="Cake"
+#read -t 10 -p "What are you?" Cake
+#Username=${Cake:="secret"}
+#echo -e "\nMY $Username!"
+#exit 0
+#================================
+#Third Test line 21
+#$Mening="Bashing is fun"
+#$printf "%s\n" "$Mening"
+
+#Bashing is fun!
+#didn't work! Help!
+#nerash@Mk3g:~/Skola/Bash$ ./lab2.sh
+#./lab2.sh: line 22: =Bashing is fun: command not found
+#./lab2.sh: line 23: fg: no job control
+
+#===============================
+#fourth Test
+$Bread=25
+$printf "price on brodband is lowerd by %d%%.\n" $Bread
+
+#nerash@Mk3g:~/Skola/Bash$ ./lab2.sh
+#./lab2.sh: line 33: =25: command not found
+#./lab2.sh: line 34: price on brodband is lowerd by %d%%.\n: command not found
+
+#It didn't work why? why does my terminal not accept printf? have the same problem at third and fourth test!
