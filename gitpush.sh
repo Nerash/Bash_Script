0fcf391ca011ecdd2afa8d4b438cf601b450f3a1
Nerash <bjorn.holm.hansen@gmail.com>
2013-12-10 10:46:54 +0100

    gitpush.sh

---
+++ gitpush.sh
@@ -0,0 +1,9 @@
+##create a git update script
+### install git-core to be able to use this script.
+Git="/usr/bin/git"
+$Git add -A
+$Git commit -m "$/skola/bash/" *[lL 1-9]
+$Git push
+#auto update this is somthing I want to achive but how? it might work with pushing it in bashrc start up script. 
+
+
