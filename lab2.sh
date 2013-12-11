49696ad8787e45cde923f93abbe9f372865663d5
Nerash <bjorn.holm.hansen@gmail.com>
2013-12-11 09:12:18 +0100

    /home/nerash/skola/bash/

--- lab2.sh
+++ lab2.sh
@@ -19,10 +19,11 @@
 #exit 0
 #================================
 #Third Test line 21
-#$Mening="Bashing is fun"
-#$printf "%s\n" "$Mening"
+#Mening="Bashing is fun"
+#printf "%s\n" "$Mening"
 
 #Bashing is fun!
+
 #didn't work! Help!
 #nerash@Mk3g:~/Skola/Bash$ ./lab2.sh
 #./lab2.sh: line 22: =Bashing is fun: command not found
@@ -30,11 +31,20 @@
 
 #===============================
 #fourth Test
-$Bread=25
-$printf "price on brodband is lowerd by %d%%.\n" $Bread
+#Bread=25
+#printf "price on brodband is lowerd by %d%%.\n" $Bread
 
 #nerash@Mk3g:~/Skola/Bash$ ./lab2.sh
 #./lab2.sh: line 33: =25: command not found
 #./lab2.sh: line 34: price on brodband is lowerd by %d%%.\n: command not found
 
 #It didn't work why? why does my terminal not accept printf? have the same problem at third and fourth test!
+
+#===============================
+#Task 1
+Username="Johan"
+echo -n Who are you?
+read -t 5 
+echo $PWD $Username
+## $PWD = print name of current/working directory
+
